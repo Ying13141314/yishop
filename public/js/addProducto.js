3c07fde990b21e339e65b41db7eb9f1f25a1ea37
YingLin <linying13141314@gmail.com>
2021-06-02 23:19:37 +0200

    controlado la talla de los productos

--- public/js/addProducto.js
+++ public/js/addProducto.js
@@ -1,13 +1,46 @@
 $(document).ready(() => {
     let talla ='';
     
+    const conTalla = $('#con-talla').val();
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: false,
+    })
+    
     $('.tallaRopa').on('click', function (e) {
         e.preventDefault();
-        talla = $(this).val()
         
+        $me = $(this);
+        
+        $('.tallaRopa').each(function (index, el) {
+            if ($(this).val() === $me.val()) {
+                $(this).removeClass('btn-light');
+                $(this).addClass('btn-dark');
+            } else {
+                $(this).removeClass('btn-dark');
+                $(this).addClass('btn-light');
+            }
+        });
+        
+        talla = $(this).val()
     })
+    
+    
     $('.add-producto').on('click', function (e) {
         e.preventDefault();
+        
+        if (conTalla && talla === '') {
+            Toast.fire({
+                icon: 'error',
+                title: 'Tiene que seleccionar una talla'
+            })
+            
+            return;
+        }
         
         const productoId = $(this).data('id');
         $.ajax({
