b0f39bb4ededb438c002de00e45ade3e09aee20a
YingLin <linying13141314@gmail.com>
2021-06-04 22:55:55 +0200

    arreglado la redireción de registrar

--- src/Controller/AdminPanelSecurityController.php
+++ src/Controller/AdminPanelSecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -21,8 +22,16 @@ class AdminPanelSecurityController extends AbstractController
     /**
      * @Route("/admin/login", name="app_login")
      */
-    public function loginTemplate(AuthenticationUtils $authenticationUtils): Response
+    public function loginTemplate(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
+        if ($referer = $request->headers->get('referer')) {
+           $referer_arr = explode('/', $referer);
+           if ( $referer_arr[count($referer_arr) - 1]  === 'registrar') {
+               return new RedirectResponse('/cliente');
+           }
+        }
+        
+        
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         $email = $authenticationUtils->getLastUsername();
